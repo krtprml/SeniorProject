e3860e66f586dbcd81fbeedc8bb682bafc4194d9
FFFames <shalom.inc@student.mahidol.ac.th>
2026-01-13 16:33:35 +0700

    update

--- Assets/Script/MainMenuManager.cs
+++ Assets/Script/MainMenuManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,25 +13,57 @@ public class MainMenuManager : MonoBehaviour
     public string gameSceneName = "House 1";
 
     public void StartGame()
-    {
-        Debug.Log("Starting game - Loading scene: " + gameSceneName);
+{
+    Debug.Log("Starting game - Loading scene: " + gameSceneName);
+    StartCoroutine(StartGameRoutine());
+}
 
-        // Load the main game scene
-        SceneManager.LoadScene(gameSceneName);
+IEnumerator StartGameRoutine()
+{
+    using (var req = new UnityEngine.Networking.UnityWebRequest(
+        "http://127.0.0.1:8000/start-game", "POST"))
+    {
+        req.downloadHandler = new UnityEngine.Networking.DownloadHandlerBuffer();
+        yield return req.SendWebRequest();
+
+        Debug.Log("Game state created on server");
     }
+
+    SceneManager.LoadScene(gameSceneName);
+}
+
+IEnumerator StartGameOnServer()
+{
+    using (var req = new UnityEngine.Networking.UnityWebRequest(
+        "http://127.0.0.1:8000/start-game", "POST"))
+    {
+        req.downloadHandler = new UnityEngine.Networking.DownloadHandlerBuffer();
+        yield return req.SendWebRequest();
+
+        Debug.Log("Game state created on server");
+    }
+}
 
     public void ExitGame()
-    {
-        Debug.Log("Exit button pressed");
+{
+    Debug.Log("Exit button pressed");
+
+    if (GameManagerSimple.I != null)
+        GameManagerSimple.I.StartCoroutine(ClearServerState());
 
 #if UNITY_EDITOR
-        // Stop playing the scene in the editor
-        EditorApplication.ExitPlaymode();
+    EditorApplication.ExitPlaymode();
 #else
-        // Quit the application in build
-        Application.Quit();
+    Application.Quit();
 #endif
-    }
+}
+
+IEnumerator ClearServerState()
+{
+    using var req = new UnityEngine.Networking.UnityWebRequest("http://127.0.0.1:8000/end-game", "POST");
+    req.downloadHandler = new UnityEngine.Networking.DownloadHandlerBuffer();
+    yield return req.SendWebRequest();
+}
 
     // Optional: Method to load any scene by name
     public void LoadScene(string sceneName)
