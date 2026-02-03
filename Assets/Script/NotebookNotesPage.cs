17f158bc928ca02f38c54f3bd73d025c0f12f7cb
Itthisak6588036 <itthisak.kam@student.mahidol.edu>
2026-02-03 16:05:39 +0700

    Updated1

--- Assets/Script/NotebookNotesPage.cs
+++ Assets/Script/NotebookNotesPage.cs
@@ -6,15 +6,21 @@ public class NotebookNotesPage : MonoBehaviour
     [Header("UI")]
     [SerializeField] TMP_InputField inputField;
 
+    void Awake()
+    {
+        // OPTIONAL: This cleans up the old "Hey it is m" message from your computer
+        PlayerPrefs.DeleteKey("DetectiveNotes");
+    }
+
     // Called when the notebook opens
     public void OnOpen()
     {
-        // Load the text from the last time you played
         if (inputField != null)
         {
-            inputField.text = PlayerPrefs.GetString("DetectiveNotes", "");
+            // We do NOT load from PlayerPrefs anymore.
+            // The text currently in the box stays there as long as the game is running.
 
-            // Move the blinking cursor to the end of the text
+            // This just puts the blinking cursor at the end of your text
             inputField.caretPosition = inputField.text.Length;
             inputField.ActivateInputField();
         }
@@ -23,11 +29,7 @@ public class NotebookNotesPage : MonoBehaviour
     // Called when the notebook closes
     public void OnClose()
     {
-        // Save the text to the computer's storage
-        if (inputField != null)
-        {
-            PlayerPrefs.SetString("DetectiveNotes", inputField.text);
-            PlayerPrefs.Save();
-        }
+        // We do NOT save to PlayerPrefs anymore.
+        // Doing nothing here means the text just sits in Unity's memory.
     }
 }
